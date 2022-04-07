0068303c54ee4f8b9fec67d0585989717acf76a3
Aleksey Landyrev <landyrev@atlasbus.ru>
2022-04-07 19:20:31 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'chart styles test' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for chart styles test
+  pod 'MapboxMaps', '10.4.2'
+
+
+end
--- chart styles test/ViewController.swift
+++ chart styles test/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import MapboxMaps
 
 class ViewController: UIViewController {
+    internal var mapView: MapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let accessToken = Bundle.main.object(forInfoDictionaryKey: "MBXAccessToken") as! String
         // Do any additional setup after loading the view.
+        let myResourceOptions = ResourceOptions(accessToken: accessToken)
+        let myMapInitOptions = MapInitOptions(
+            resourceOptions: myResourceOptions,
+            styleURI: .init(url: .init(string: "")!)!
+        )
+        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+         
+        self.view.addSubview(mapView)
     }
 
 
